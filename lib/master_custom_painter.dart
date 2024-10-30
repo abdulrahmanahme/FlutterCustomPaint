8d0393d1af46a2b41318b559917d931eef6e39c5
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-10-30 21:19:09 +0300

    -  Draw a Rectangle

--- lib/master_custom_painter.dart
+++ lib/master_custom_painter.dart
@@ -11,8 +11,22 @@ class MasterCustomPainter extends CustomPainter {
     // canvas.drawLine(Offset.zero, Offset(size.width,0), paint);
 
     /// Draw a circle
+    // paint.style = PaintingStyle.stroke;
+    // canvas.drawCircle(Offset(size.width / 2, size.width / 2), 40, paint);
+
+    /// Draw a Rectangle
+
     paint.style = PaintingStyle.stroke;
-    canvas.drawCircle(Offset(size.width / 2, size.width / 2), 40, paint);
+
+    Offset center = Offset(size.width / 2, size.width / 2);
+    // canvas.drawRect(
+    //     Rect.fromCenter(center: center, width: 100, height: 100), paint);
+
+    canvas.drawRect(Rect.fromCircle(center: center, radius: 20), paint);
+
+    canvas.drawRect(const Rect.fromLTWH(10, 50, 20, 200), paint);
+    canvas.drawRect(const Rect.fromLTWH(94, 60, 20, 200), paint);
+
   }
 
   @override
