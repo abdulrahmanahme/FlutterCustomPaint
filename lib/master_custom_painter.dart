18acb449ae99c4526b68c3ac07e03d2360d7eafb
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-10-30 21:09:20 +0300

    - Draw a circle

--- lib/master_custom_painter.dart
+++ lib/master_custom_painter.dart
@@ -1,14 +1,18 @@
-
 import 'package:flutter/material.dart';
 
-class MasterCustomPainter extends CustomPainter{
+class MasterCustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint =Paint();
-    paint.strokeWidth=8;
-    paint.color=Colors.black;
+    Paint paint = Paint();
+    paint.strokeWidth = 3;
+    paint.color = Colors.black;
 
-    canvas.drawLine(Offset.zero, Offset(size.width,0), paint);
+    /// Draw a line
+    // canvas.drawLine(Offset.zero, Offset(size.width,0), paint);
+
+    /// Draw a circle
+    paint.style = PaintingStyle.stroke;
+    canvas.drawCircle(Offset(size.width / 2, size.width / 2), 40, paint);
   }
 
   @override
