1c1ec8047fb74d65e935c9bfb119160e7bf1e740
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-11-10 15:22:53 +0200

    - add DrawingCustomPainter

--- lib/drawing_app/drawing_app_screen.dart
+++ lib/drawing_app/drawing_app_screen.dart
@@ -18,8 +18,27 @@ class _DrawingAppScreenState extends State<DrawingAppScreen> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
+          color: Colors.red[50],
+          child: CustomPaint(
+            painter: DrawingCustomPainter(),
+            size: Size(MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height),
+          ),
         ),
       ),
     );
+  }
+}
+
+class DrawingCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
   }
 }
